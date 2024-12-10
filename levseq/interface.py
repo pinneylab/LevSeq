a0c5c3a9ae66540b8b3a8f1b021c4c12278a8a16
jzhang1198 <jzhang1198@gmail.com>
2024-12-10 15:28:32 -0800

    Added checks for config file

--- levseq/interface.py
+++ levseq/interface.py
@@ -38,6 +38,27 @@ def build_cli_parser():
     return parser
 
 
+def check_config():
+
+    assert config['front_window_size'] > 0 and isinstance(config['front_window_size'], int), 'front_window_size must be an integer greater than 0.'
+    assert config['rear_window_size'] > 0 and isinstance(config['rear_window_size'], int), 'rear_window_size must be an integer greater than 0.'
+
+    assert config['min_read_length'] > 0 and isinstance(config['min_read_length'], int), 'min_read_length must be an integer greater than 0.'
+    assert config['max_read_length'] > 0 and isinstance(config['max_read_length'], int), 'max_read_length must be an integer greater than 0.'
+    assert config['min_read_length'] < config['max_read_length'], 'min_read_length cannot be greater than max_read_length'
+
+    assert config['alignment_score_threshold'] <= 100 and config['alignment_score_threshold'] >= 0, 'alignment_score_threshold must be a number between 0 and 100.'
+    assert config['edit_distance_threshold'] > 0 and isinstance(config['edit_distance_threshold'], int), 'edit_distance_threshold must be an integer greater than 0.'
+
+    assert isinstance(config['position_offset'], int), 'position_offset must be an integer.'
+    assert config['calling_threshold'] >= 0 and config['calling_threshold'] <= 1, 'calling_threshold must be a number between 0 and 1.'
+    assert config['n_threads'] >= 0 and isinstance(config['n_threads'], int), 'n_threads must be an integer greater than or equal to 0.'
+
+    assert isinstance(config['skip_demultiplexing'], bool), 'skip_demultiplexing must be a boolean.'
+    assert isinstance(config['skip_variantcalling'], bool), 'skip_variantcalling must be a boolean.'
+    assert isinstance(config['show_msa'], bool), 'show_msa must be a boolean.'
+
+
 # Execute LevSeq
 def execute_LevSeq():
     # Build parser
@@ -46,7 +67,7 @@ def execute_LevSeq():
 
     with open(args['config'], 'r') as f:
         config = yaml.safe_load(f)
-
+    check_config(config)
     # Parse the arguments
     # Set up progres bar
     tqdm_fn = tqdm.tqdm
